96791ff7a177252643548855e2f8de6765ad27c2
unknown <e2tkdwls1@gmail.com>
2025-10-26 10:02:25 +0900

    Updates

---
+++ _next/static/chunks/028e8876015a106a.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/fb6349162b103c66.js",
+  "static/chunks/turbopack-70a534314bbf724c.js"
+])
---
+++ _next/static/d0Ryo7qAxtjWxue1rb23x/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/94ca4d3ea9379857.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/CSJ_portfolio//_next/:path+",
+        "destination": "/CSJ_portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
